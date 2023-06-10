6146bed3b08f1004312d0a3b9b1423be3753ea50
Jose110903 <jose131923@outlook.es>
2023-06-10 14:34:50 -0500

    Add models Account

--- src/main/java/com/example/Example3/model/Account.java
+++ src/main/java/com/example/Example3/model/Account.java
@@ -1,4 +1,31 @@
 package com.example.Example3.model;
 
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.message.AsynchronouslyFormattable;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+
+
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name_customer", length = 22, nullable = false)
+    private String nameCustomer;
+    @Column (name="number_account", length = 13, nullable = false)
+    private String numberAccount;
+
+
 }
